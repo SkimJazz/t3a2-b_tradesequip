336f1123c94b57fe4ea97c45ec990d1266a9671f
SkimJazz <jb.skimjazz@outlook.com>
2024-08-03 16:18:45 +1000

    Refactored `Login.jsx` and `App.jsx`
    - Changed name child path from register to signup in `App.jsx` and Link in `login.jsx`

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,7 @@ const router = createBrowserRouter([
                 element: <Landing />,
             },
             {
-                path: 'register',
+                path: 'signup',
                 element: <Signup />,
             },
             {
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,7 +4,7 @@ const Login = () => {
     return (
         <div>
             <h1>Login</h1>
-            <Link to='/register'>Sign Up Page</Link>
+            <Link to='/signup'>Sign Up Page</Link>
         </div>
     );
 };
