f6a2c9da1b904cc8073852e2b75c53926dd92677
SkimJazz <jb.skimjazz@outlook.com>
2024-08-09 11:32:53 +1000

    Setup database connection and job model
    - Configure MongoDB connection in `server.js` using environment variables.
    - Add `JobModel.js` with schema definition for job data.
    - Ensure `.env` file includes `MONGO_URL` for database connection.
    - Update `jobController.js` to interact with the `Job` model.
    
    MongoDB database connection and initial job model operations are good to go!

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,15 +1,9 @@
-import { nanoid } from 'nanoid';
+import Job from '../models/JobModel.js';
 
 
-// Array of jobs
-let myJobs = [
-    { id: nanoid(), clientName: 'hutchinson', jobTitle: 'formwork' },
-    { id: nanoid(), clientName: 'metricon', jobTitle: 'concreting' },
-];
-
-
-// GET ALL JOBS IN MY LIST
+// GET ALL MY JOBS IN LIST
 export const getMyJobs = async (req, res) => {
+    const myJobs = await Job.find({});
     res.status(200).json({ myJobs });
 };
 
@@ -17,9 +11,9 @@ export const getMyJobs = async (req, res) => {
 // GET JOB BY ID
 export const getJobById = async (req, res) => {
     const { id } = req.params;
-    const job = myJobs.find((job) => job.id === id);
+    const job = await Job.findById(id);
+    console.log(job);
     if (!job) {
-        // throw new Error('no job with that id');
         return res.status(404).json({ msg: `no job with id ${id}` });
     }
     res.status(200).json({ job });
@@ -29,42 +23,30 @@ export const getJobById = async (req, res) => {
 // CREATE NEW JOB
 export const createNewJob = async (req, res) => {
     const { clientName, jobTitle } = req.body;
-    if (!clientName || !jobTitle) {
-        return res.status(400).json({ msg: 'Provide Client Name and Job Title' });
-    }
-    const id = nanoid(10);
-    const job = { id, clientName, jobTitle };
-    myJobs.push(job);
-    res.status(200).json({ job });
+    const job = await Job.create({ clientName, jobTitle });
+    res.status(201).json({ job });
 };
 
 
 // UPDATE JOB BY ID
 export const updateJobById = async (req, res) => {
-    const { clientName, jobTitle } = req.body;
-    if (!clientName || !jobTitle) {
-        return res.status(400).json({ msg: 'provide Client Name and Job Title' });
-    }
     const { id } = req.params;
-    const job = myJobs.find((job) => job.id === id);
-    if (!job) {
+    const updateJob = await Job.findByIdAndUpdate(id, req.body, {
+        new: true,
+    });
+    if (!updateJob) {
         return res.status(404).json({ msg: `no job with id ${id}` });
     }
-    job.clientName = clientName;
-    job.jobTitle = jobTitle;
-    res.status(200).json({ msg: 'job modified', job });
+    res.status(200).json({ msg: 'job modified', job: updateJob });
 };
 
 
-
+// DELETE JOB BY ID
 export const deleteJobById = async (req, res) => {
     const { id } = req.params;
-    const job = myJobs.find((job) => job.id === id);
-    if (!job) {
+    const removeJob = await Job.findByIdAndDelete(id);
+    if (!removeJob) {
         return res.status(404).json({ msg: `no job with id ${id}` });
     }
-    const newJobs = myJobs.filter((job) => job.id !== id);
-    myJobs = newJobs;
-
-    res.status(200).json({ msg: 'job deleted' });
+    res.status(200).json({ msg: 'job deleted', job: removeJob });
 };
