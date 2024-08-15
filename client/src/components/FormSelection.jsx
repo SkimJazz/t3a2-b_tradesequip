f5d836a1157a2ae7af6369698af8176a0ea1eb61
SkimJazz <jb.skimjazz@outlook.com>
2024-08-15 17:28:38 +1000

    feat: Setup job structure and implement form handling
    - Added `NewJob` component with form handling for creating new jobs.
    - Implemented `reactRouterAction` in `NewJob.jsx` to handle form submission and job creation.
    - Created `FormSelection` component for rendering select dropdown fields.
    - Updated `App.jsx` to include `newJobAction` in the route configuration.
    - Styled the job form using `FormDashboard.js`.

---
+++ client/src/components/FormSelection.jsx
@@ -0,0 +1,46 @@
+/**
+ * @description FormRowSelect is a functional component that renders a select dropdown field.
+ *
+ * @component
+ * @param {Object} props - Properties passed to the component.
+ * @param {string} props.name - Name and id of the select dropdown.
+ * @param {string} props.labelText - Text for the label of the select dropdown.
+ * @param {Array} props.list - Array of values that the select dropdown should display as options.
+ * @param {string} [props.defaultValue=''] - Initial value that the select dropdown should display.
+ *
+ * @link {AddJob.jsx} - Data passed to this component is defined and managed in the AddJob.jsx file.
+ *
+ * @example
+ * <FormRowSelect
+ *    name='jobStatus'
+ *    labelText='Job Status'
+ *    list={['Pending', 'In Progress', 'Completed', 'Cancelled']}
+ *    defaultValue='Pending'
+ * />
+ */
+const FormSelection = ({ name, labelText, list, defaultValue = ' ', onChange }) => {
+    return (
+        <div className="form-row">
+            <label htmlFor={name} className='form-label'>
+                {labelText || name}
+            </label>
+            <select
+                name={name}
+                id={name}
+                className='form-select'
+                defaultValue={defaultValue}
+                // onChange event handler for select dropdown
+                onChange={onChange}
+            >
+                {list.map((itemValue) => {
+                    return (
+                        <option key={itemValue} value={itemValue}>
+                            {itemValue}
+                        </option>
+                    );
+                })}
+            </select>
+        </div>
+    );
+}
+export default FormSelection
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { default as FormRow } from './FormRow';
 export { default as LargeSidebar } from './LargeSidebar';
 export { default as SmallSidebar } from './SmallSidebar';
 export { default as Navbar } from './Navbar';
+export { default as FormSelection } from './FormSelection.jsx';
