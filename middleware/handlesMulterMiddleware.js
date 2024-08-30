e3b3fe3368b82f1fc9faea3417d7e32bff41f180
SkimJazz <jb.skimjazz@outlook.com>
2024-08-30 17:40:38 +1000

    Update user profile handling and Cloudinary image uploads
    - Implemented image formatting function using `datauri` in `handlesMulterMiddleware.js`
    - Updated `userController.js` to handle user profile updates, image uploads to Cloudinary

--- middleware/handlesMulterMiddleware.js
+++ middleware/handlesMulterMiddleware.js
@@ -1,19 +1,18 @@
 import multer from 'multer';
+import DataParser from 'datauri/parser.js';
+import path from 'path';
 
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        // set the directory where uploaded files will be stored
-        cb(null, 'public/uploads');
-    },
+const storage = multer.memoryStorage();
 
-    // Keep the original file name
-    filename: (req, file, cb) => {
-        const fileName = file.originalname;
-        // set the name of the uploaded file
-        cb(null, fileName);
-    },
-});
 const upload = multer({ storage });
 
+const parser = new DataParser();
+
+export const formatImage = (file) => {
+    console.log(file);
+    const fileExtension = path.extname(file.originalname).toString();
+    return parser.format(fileExtension, file.buffer).content;
+
+};
 export default upload;
